41f009a246d70bb3549cac04d91822cc37c2f7ab
Ibrahim Al-harthi <ibrakh80@gmail.com>
2025-08-24 17:31:18 +1000

    updating cv and info

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div id="About" data-aos="zoom-in">
         <h1 className='AboutName'><i className='Hi'>Hi,</i> I’m Ibrahim Alharthi.</h1>
         <p className='Details'>
-        I'm a Computer Engineering graduate and a current Master's student in Machine Learning and Computer Vision at ANU. I’m skilled in Full Stack development with Node.js on the backend, React and Angular on the frontend, and have a strong command of MySQL and PostgreSQL databases, along with hands-on experience in AWS cloud projects.
+        I'm a Computer Engineering graduate and a current Master's student in Machine Learning and Computer Vision at ANU.  <br/>I’m skilled in Full Stack development with Node.js on the backend, React and Angular on the frontend, and have a strong command of MySQL and PostgreSQL databases, along with hands-on experience in AWS cloud projects, while currently working on Computer Vision and Machine Learning projects that combine AI research with practical applications.
         </p>
     </div>
   )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,9 +4,6 @@ const Main = () => {
   return (
     <div id='Main'>
         <div className="MainPicture">
-            <div className="MainPattern" data-aos="fade-up">
-                <img src="/images/MyPicture.svg" alt="My Picture" className='pattern' />
-            </div>
             <div className="TextAnimation">
               <img src="/images/vectors/Text.svg" alt="ibrahim portfolio" className="MainTextImage" draggable="false"/>
               <img src="/images/vectors/Dots.svg" alt="Dots" className="MainDots" draggable="false"/>
